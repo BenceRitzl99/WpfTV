add22facc1fd7e3d76bb189686579b221ef579af
Tanulo <you@example.com>
2024-05-27 12:14:51 +0200

    órai 2024-05-27

---
+++ Pages/Element.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTV.Pages
+{
+    /// <summary>
+    /// Interaction logic for Element.xaml
+    /// </summary>
+    public partial class Element : UserControl
+    {
+        public string Kind
+        {
+            get
+            {
+                return (string)GetValue(KindProperty);
+            }
+
+
+            set
+            {
+                SetValue(KindProperty, value);
+            }
+        }
+        public static DependencyProperty KindProperty = DependencyProperty.Register("Kind", typeof(string), typeof(Element));
+        
+        public string Title
+        { 
+             
+        
+            get
+            {
+                return (string)GetValue(TitleProperty);
+            }
+
+
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+        public static DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Element));
+        public Element()
+        {
+            //this.Kind = "Wifi";
+            //this.Title = "Network Settings";
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ Pages/StorageTitlePage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTV.Pages
+{
+    /// <summary>
+    /// Interaction logic for StorageTitlePage.xaml
+    /// </summary>
+    public partial class StorageTitlePage : UserControl
+    {
+        public Brush Color
+        {
+            get
+            {
+                return (Brush)GetValue(ColorProperty);
+            }
+
+
+            set
+            {
+                SetValue(ColorProperty, value);
+            }
+        }
+        public static DependencyProperty ColorProperty = DependencyProperty.Register("Color", typeof(Brush), typeof(StorageTitlePage));
+
+        public string Title
+        {
+
+
+            get
+            {
+                return (string)GetValue(TitleProperty);
+            }
+
+
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+        public static DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(StorageTitlePage));
+        public StorageTitlePage()
+        {
+            //this.Kind = "Wifi";
+            //this.Title = "Network Settings";
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
